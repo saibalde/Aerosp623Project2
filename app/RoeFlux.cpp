7f3a776799bb46386b71b7299abf2b5d26441acf
Saibal De <saibal.de.bnk@gmail.com>
2019-02-20 23:04:16 -0500

    Fix Roe flux supersonic test

--- app/RoeFlux.cpp
+++ app/RoeFlux.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <cstdlib>
+#include <cmath>
 
 #include <armadillo>
 
@@ -12,96 +13,110 @@ double uniform(double a, double b)
     return a + ((b - a) * std::rand() / RAND_MAX);
 }
 
-void consistencyTest(double gamma, const EulerDefaultBase &problem)
+double consistencyTestError(double gamma, const EulerDefaultBase &problem)
 {
+    // construct random unit normal
+    const double nx = uniform(-1.0, 1.0);
+    const double ny = uniform(-1.0, 1.0);
+    const double nl = std::hypot(nx, ny);
+    const arma::rowvec n {nx / nl, ny / nl};
+
+    // construct a state
     const double rho = uniform( 0.0, 1.0);
-    const double u   = uniform(-1.0, 1.0);
-    const double v   = uniform(-1.0, 1.0);
-    const double e   = uniform( 0.0, 1.0);
-
-    const double nx  = uniform(-1.0, 1.0);
-    const double ny  = uniform(-1.0, 1.0);
-    const double nl  = std::hypot(nx, ny);
-
+    const double u = uniform(-1.0, 1.0);
+    const double v = uniform(-1.0, 1.0);
+    const double e = uniform( 0.0, 1.0);
     const double E = e + 0.5 * (u * u + v * v);
+    const arma::vec U {rho, rho * u, rho * v, rho * E};
+
+    // compute analytical flux
     const double p = (gamma - 1.0) * rho * e;
     const double H = E + p / rho;
-
-    const arma::vec U {rho, rho * u, rho * v, rho * E};
-    const arma::rowvec n {nx / nl, ny / nl};
     const arma::vec F {
             n[0] * (rho * u)         + n[1] * (rho * v),
             n[0] * (rho * u * u + p) + n[1] * (rho * u * v),
             n[0] * (rho * u * v)     + n[1] * (rho * v * v + p),
             n[0] * (rho * u * H)     + n[1] * (rho * v * H)};
 
+    // compute Roe flux
     arma::vec FHat(4);
     double s;
     problem.computeRoeFlux(U, U, n, FHat, s);
 
-    const double error = arma::norm(F - FHat);
-
-    std::cout << "Consistency test: L2 error = " << error << std::endl;
+    // return error
+    return arma::norm(F - FHat);
 }
 
-void flipTest(const EulerDefaultBase &problem)
+double flipTestError(const EulerDefaultBase &problem)
 {
-    const double rhoL = uniform( 0.0, 1.0);
-    const double uL   = uniform(-1.0, 1.0);
-    const double vL   = uniform(-1.0, 1.0);
-    const double EL   = uniform( 0.0, 1.0) + 0.5 * (uL * uL + vL * vL);
+    // construct random oppsing unit normals
+    const double nx = uniform(-1.0, 1.0);
+    const double ny = uniform(-1.0, 1.0);
+    const double nl = std::hypot(nx, ny);
+    const arma::rowvec nLR { nx / nl,  ny / nl};
+    const arma::rowvec nRL {-nx / nl, -ny / nl};
 
+    // construct left state
+    const double rhoL = uniform( 0.0, 1.0);
+    const double uL = uniform(-1.0, 1.0);
+    const double vL = uniform(-1.0, 1.0);
+    const double EL = uniform( 0.0, 1.0) + 0.5 * (uL * uL + vL * vL);
+    const arma::vec UL {rhoL, rhoL * uL, rhoL * vL, rhoL * EL};
+
+    // construct right state
     const double rhoR = uniform( 0.0, 1.0);
     const double uR   = uniform(-1.0, 1.0);
     const double vR   = uniform(-1.0, 1.0);
     const double ER   = uniform( 0.0, 1.0) + 0.5 * (uR * uR + vR * vR);
-
-    const double nx  = uniform(-1.0, 1.0);
-    const double ny  = uniform(-1.0, 1.0);
-    const double nl  = std::hypot(nx, ny);
-
-    const arma::vec UL {rhoL, rhoL * uL, rhoL * vL, rhoL * EL};
     const arma::vec UR {rhoR, rhoR * uR, rhoR * vR, rhoR * ER};
 
-    const arma::rowvec nLR { nx / nl,  ny / nl};
-    const arma::rowvec nRL {-nx / nl, -ny / nl};
-
-    arma::vec FHatLR(4);
-    arma::vec FHatRL(4);
+    // temporary variable to store wave speed, not used here!
     double s;
 
+    // compute Roe flux from left to right
+    arma::vec FHatLR(4);
     problem.computeRoeFlux(UL, UR, nLR, FHatLR, s);
+
+    // compute Roe flux from right to left
+    arma::vec FHatRL(4);
     problem.computeRoeFlux(UR, UL, nRL, FHatRL, s);
 
-    const double error = arma::norm(FHatLR + FHatRL);
-
-    std::cout << "Flip test: L2 error = " << error << std::endl;
+    // return error
+    return arma::norm(FHatLR + FHatRL);
 }
 
-void supersonicTest(double gamma, const EulerDefaultBase &problem)
+double supersonicTestError(double gamma, const EulerDefaultBase &problem)
 {
-    const double rhoL = uniform( 0.0, 1.0);
-    const double uL   = 0.8;
-    const double vL   = 0.0;
-    const double eL   = uniform( 0.0, 1.0);
+    // construct normal that points north-east
+    const double nx = uniform(0.0, 1.0);
+    const double ny = uniform(0.0, 1.0);
+    const double nl = std::hypot(nx, ny);
+    const arma::rowvec n {nx / nl, ny / nl};
 
-    const double rhoR = uniform( 0.0, 1.0);
-    const double uR   = uniform(-1.0, 0.0);
-    const double vR   = uniform(-1.0, 1.0);
-    const double eR   = uniform( 0.0, 1.0);
-
-    const double EL = eL + 0.5 * (uL * uL + vL * vL);
-    const double ER = eR + 0.5 * (uR * uR + vR * vR);
+    // construct supersonic state on the left
+    const double rhoL = uniform(0.0, 1.0);
+    const double uL   = uniform(0.8, 1.0);
+    const double vL   = uniform(0.8, 1.0);
+    const double eL   = uniform(0.0, 1.0);
+    const double EL   = eL + 0.5 * (uL * uL + vL * vL);
 
     const arma::vec UL {rhoL, rhoL * uL, rhoL * vL, rhoL * EL};
+
+    // construct supersonic state on the right
+    const double rhoR = uniform(0.0, 1.0);
+    const double uR   = uniform(0.8, 1.0);
+    const double vR   = uniform(0.8, 1.0);
+    const double eR   = uniform(0.0, 1.0);
+    const double ER   = eR + 0.5 * (uR * uR + vR * vR);
+
     const arma::vec UR {rhoR, rhoR * uR, rhoR * vR, rhoR * ER};
 
-    const arma::rowvec n {1.0, 0.0};
-
+    // compute Roe flux
     arma::vec FHat(4);
     double s;
     problem.computeRoeFlux(UL, UR, n, FHat, s);
 
+    // compute analytical flux on the left
     const double pL = (gamma - 1.0) * rhoL * eL;
     const double HL = EL + pL / rhoL;
 
@@ -111,23 +126,30 @@ void supersonicTest(double gamma, const EulerDefaultBase &problem)
             n[0] * (rhoL * uL * vL)      + n[1] * (rhoL * vL * vL + pL),
             n[0] * (rhoL * uL * HL)      + n[1] * (rhoL * vL * HL)};
 
-    const double error = arma::norm(FHat - FL);
-
-    std::cout << "Supersonic test: L2 error = " << error << std::endl;
+    // return error
+    return arma::norm(FHat - FL);
 }
 
 int main()
 {
-    std::srand(0);
+    // random seed
+    std::srand(1279);
 
-    double gamma = 1.4;
+    // this choice is gamma is very important for supersonic test!
+    const double gamma = 1.4;
 
+    // set up problem
     EulerDefaultBase problem;
     problem.setSpecificHeatRatio(gamma);
 
-    consistencyTest(gamma, problem);
-    flipTest(problem);
-    supersonicTest(gamma, problem);
+    // report errors
+    std::cout << "L2 errors in" << std::endl
+              << "  Consistency test: " << std::scientific
+              << consistencyTestError(gamma, problem) << std::endl
+              << "  Flip test       : " << std::scientific
+              << flipTestError(problem) << std::endl
+              << "  Supersonic test : " << std::scientific
+              << supersonicTestError(gamma, problem) << std::endl;
 
     return 0;
 }
